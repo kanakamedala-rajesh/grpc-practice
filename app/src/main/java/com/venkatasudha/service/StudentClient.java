a504239f5ba831af082df23a26aedd7968e8195a
Rajesh Kanakamedala <kanakamedala.rajesh@gmail.com>
2022-01-16 06:52:04 +0530

    Added all available stub types and streams

--- app/src/main/java/com/venkatasudha/service/StudentClient.java
+++ app/src/main/java/com/venkatasudha/service/StudentClient.java
@@ -1,23 +1,56 @@
 package com.venkatasudha.service;
 
+import com.google.protobuf.Empty;
 import com.google.protobuf.Int32Value;
 import com.venkatasudha.entities.EntitiesProto;
 import com.venkatasudha.services.StudentServiceGrpc;
 import io.grpc.Channel;
+import io.grpc.stub.StreamObserver;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 public class StudentClient {
     private final StudentServiceGrpc.StudentServiceBlockingStub blockingStub;
+    private final StudentServiceGrpc.StudentServiceStub studentServiceStub;
 
     public StudentClient(Channel channel) {
         blockingStub = StudentServiceGrpc.newBlockingStub(channel);
+        studentServiceStub = StudentServiceGrpc.newStub(channel);
     }
 
     public void getStudent(int id) {
+        StreamObserver<EntitiesProto.Student> studentStreamObserver = new StreamObserver<EntitiesProto.Student>() {
+            @Override
+            public void onNext(EntitiesProto.Student value) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+
         EntitiesProto.Student student = blockingStub.getStudentByID(Int32Value.newBuilder().setValue(id).build());
 
         System.out.println("ID: " + student.getId());
         System.out.println("Name: " + student.getName());
         System.out.println("Group: " + student.getGroup());
         System.out.println("Hash Code: " + student.hashCode());
+
+
+        blockingStub.getStudent(Empty.newBuilder().build());
+        Iterator<EntitiesProto.Student> studentList = blockingStub.getStudentList(Int32Value.newBuilder().setValue(id).build());
+        studentList.forEachRemaining(student1 -> {
+            System.out.println("Received StudentName: "+student1.getName());
+        });
+        studentServiceStub.getStudentByStream(studentStreamObserver);
+        studentServiceStub.getStudentsListByStream(studentStreamObserver);
     }
 }
--- app/src/main/java/com/venkatasudha/service/StudentServerImpl.java
+++ app/src/main/java/com/venkatasudha/service/StudentServerImpl.java
@@ -7,21 +7,75 @@ import com.venkatasudha.services.StudentServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
 public class StudentServerImpl extends StudentServiceGrpc.StudentServiceImplBase {
+    StreamObserver<Int32Value> int32ValueStreamObserver = new StreamObserver<>() {
+        @Override
+        public void onNext(Int32Value value) {
+            System.out.println("OnNext: " + value);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            System.err.println("int32ValueStreamObserver error: " + t.getLocalizedMessage());
+        }
+
+        @Override
+        public void onCompleted() {
+            System.out.println("onCompleted");
+        }
+    };
+
+    StreamObserver<Int32Value> int32ValueStreamObserver2 = new StreamObserver<>() {
+        @Override
+        public void onNext(Int32Value value) {
+            System.out.println("OnNext2: " + value);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+            System.err.println("int32ValueStreamObserver error2: " + t.getLocalizedMessage());
+        }
+
+        @Override
+        public void onCompleted() {
+            System.out.println("onCompleted2");
+        }
+    };
+
     @Override
     public void getStudent(Empty request, StreamObserver<EntitiesProto.Student> responseObserver) {
-        super.getStudent(request, responseObserver);
+        System.out.println("getStudent: ");
+        EntitiesProto.Student responseStudent = getStudent(-1);
+        responseObserver.onNext(responseStudent);
+        responseObserver.onCompleted();
     }
 
     @Override
     public void getStudentByID(Int32Value request, StreamObserver<EntitiesProto.Student> responseObserver) {
         System.out.println("getStudentByID: " + request.getValue());
-        EntitiesProto.Student responseStudent = EntitiesProto.Student.newBuilder()
-              .setId(request.getValue())
-              .setName("Rajesh Kanakamedala")
-              .setGroup(EntitiesProto.Group.M_TECH)
-              .build();
-
-        responseObserver.onNext(responseStudent);
+        responseObserver.onNext(getStudent(request.getValue()));
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getStudentList(Int32Value request, StreamObserver<EntitiesProto.Student> responseObserver) {
+        System.out.println("getStudentList: " + request.getValue());
+        responseObserver.onNext(getStudent(request.getValue()));
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<Int32Value> getStudentByStream(StreamObserver<EntitiesProto.Student> responseObserver) {
+        System.out.println("getStudentByStream: ");
+        return int32ValueStreamObserver;
+    }
+
+    @Override
+    public StreamObserver<Int32Value> getStudentsListByStream(StreamObserver<EntitiesProto.Student> responseObserver) {
+        System.out.println("getStudentsListByStream: ");
+        return int32ValueStreamObserver2;
+    }
+
+    private EntitiesProto.Student getStudent(int request) {
+        return EntitiesProto.Student.newBuilder().setId(request).setName("Rajesh Kanakamedala").setGroup(EntitiesProto.Group.M_TECH).build();
     }
 }
